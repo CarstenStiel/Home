fa4d3032f5fe9b504b436a90ebee9b0f0d5c1da6
CarstenStiel <timothy.kriewald@gmail.com>
2023-06-12 22:14:09 +0200

    Reuseable Button

---
+++ src/components/buttons/defaultButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="callFunction" class="bg-slate-200 rounded full px-2 ease-in duration-200 hover:bg-slate-400">Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    myFunction: {
+      type: Function,
+      required: true
+    }
+  },
+  methods: {
+    callFunction() {
+      this.myFunction(); // Funktion aufrufen
+    }
+  }
+};
+</script>
--- src/views/DeviceView.vue
+++ src/views/DeviceView.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="flex items-center justify-center h-screen flex-col">
     <div class="text-xl text-center">Hier sollen die Geräte eingestellt werden können!</div>
-    <Button>Test</Button>
+    <Button :my-function='toggleDiv'>Test</Button>
+    <div v-if="showDiv" class="text-center">Test</div>
   </div>
 </template>
 
 <script>
-import Button from "@/components/buttonTest.vue";
+import Button from "@/components/buttons/defaultButton.vue";
 
 export default {
   components: { Button },
+  data() {
+    return {
+      showDiv: false
+    };
+  },
+  methods: {
+    toggleDiv() {
+      this.showDiv = !this.showDiv;
+    }
+  }
 };
 </script>
